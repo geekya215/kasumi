d8a3072a470606fe9029afb1ed38399dc37b25fa
Lin Yang <geekya215@gmail.com>
2022-02-25 16:39:53 +0800

    Hello kasumi!

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    java
+}
+
+group = "io.geekya"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
